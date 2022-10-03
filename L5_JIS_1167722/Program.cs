a3e4cec94a6367d59be78c7ba9780d8a5d27658a
Estudiante <Estudiante@T203-13-2022>
2022-10-03 20:47:04 +0200

    Agregar archivos de proyecto.

---
+++ L5_JIS_1167722/Program.cs
@@ -0,0 +1,25 @@
+﻿int numeroEntero;
+Console.WriteLine("Ejercicio 1");
+Console.WriteLine("Número entero");
+Console.WriteLine("Por favor, ingrese un número entero");
+numeroEntero = int.Parse(Console.ReadLine());
+{
+    if (numeroEntero >= 1)
+    {
+        Console.WriteLine("numero positivo");
+    }
+
+    if (numeroEntero <= -1)
+    {
+        Console.WriteLine("numero negativo");
+    }
+    if (numeroEntero == 0)
+    {
+        Console.WriteLine("cero");
+
+    }
+
+
+    Console.ReadKey();
+    Console.WriteLine(numeroEntero);
+}
